dcd6ded50f26b61b1be8956f8cd7896f582c3c69
kandy53719 <xuze53719@163.com>
2023-03-12 20:04:44 +0800

    测试

--- study/micro/main.go
+++ study/micro/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func prof() {
+	panic("ok")
+}
+
+func main() {
+	var t = time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-t.C:
+			//fmt.Println(time.Now())
+			go func() {
+				defer func() {
+					if err := recover(); err != nil {
+						fmt.Println(time.Now(), err)
+					}
+				}()
+				prof()
+			}()
+		}
+	}
+}
