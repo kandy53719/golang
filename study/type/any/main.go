7d619f993fc3d4f8b9a0c3443153080624226d64
kandy53719 <xuze53719@163.com>
2023-03-15 13:30:47 +0800

    测试

--- study/type/any/main.go
+++ study/type/any/main.go
@@ -12,6 +12,27 @@ func PrintList[T any](list []T) {
 //泛型类型
 type list[T any] []T
 
+//自定义类型
+type CusType interface {
+	int | float64 | string
+}
+
+// 原始求和
+func Sum(m map[string]int) (sum int) {
+	for _, v := range m {
+		sum += v
+	}
+	return
+}
+
+// 泛型求和
+func SumAny[K comparable, V CusType](m map[K]V) (sum V) {
+	for _, v := range m {
+		sum += v
+	}
+	return
+}
+
 func main() {
 	PrintList([]int{1, 2, 3})
 	PrintList([]string{"张三", "李四", "王五"})
